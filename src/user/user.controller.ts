0c99e5c9a1ff55a755bb242cf1154cdedc7b0bda
Ashutosh Shelke <ashutoshshelke07@gmail.com>
2023-11-19 04:46:12 +0530

    get user by id

---
+++ src/user/user.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('user')
+export class UserController {
+
+    @Get(':id')
+    findAll(@Param('id') id:string) {
+        return ({
+            'user': {
+                'id': id
+            }
+        });
+    }
+
+}
